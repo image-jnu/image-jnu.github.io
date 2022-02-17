697aae0737b395408b611be36afb27751a421d1e
나광일 <nagwangil@MacBookAir.local>
2022-02-17 20:47:08 +0900

    update route name
    actual routes are not changed yet

--- src/routes.js
+++ src/routes.js
@@ -43,9 +43,9 @@ import Icon from "@mui/material/Icon";
 import GitHubIcon from "@mui/icons-material/GitHub";
 
 // Pages
-import AboutUs from "layouts/pages/landing-pages/about-us";
-import ContactUs from "layouts/pages/landing-pages/contact-us";
-import Author from "layouts/pages/landing-pages/author";
+// import AboutUs from "layouts/pages/landing-pages/about-us";
+// import ContactUs from "layouts/pages/landing-pages/contact-us";
+// import Author from "layouts/pages/landing-pages/author";
 import SignIn from "layouts/pages/authentication/sign-in";
 
 // Sections
@@ -56,9 +56,9 @@ import NavTabs from "layouts/sections/navigation/nav-tabs";
 import Pagination from "layouts/sections/navigation/pagination";
 import Inputs from "layouts/sections/input-areas/inputs";
 import Forms from "layouts/sections/input-areas/forms";
-import Alerts from "layouts/sections/attention-catchers/alerts";
-import Modals from "layouts/sections/attention-catchers/modals";
-import TooltipsPopovers from "layouts/sections/attention-catchers/tooltips-popovers";
+// import Alerts from "layouts/sections/attention-catchers/alerts";
+// import Modals from "layouts/sections/attention-catchers/modals";
+// import TooltipsPopovers from "layouts/sections/attention-catchers/tooltips-popovers";
 import Avatars from "layouts/sections/elements/avatars";
 import Badges from "layouts/sections/elements/badges";
 import BreadcrumbsEl from "layouts/sections/elements/breadcrumbs";
@@ -70,30 +70,14 @@ import Typography from "layouts/sections/elements/typography";
 
 const routes = [
   {
-    name: "pages",
+    name: "Lab Info",
     icon: <Icon>dashboard</Icon>,
     columns: 1,
     rowsPerColumn: 2,
     collapse: [
       {
         name: "landing pages",
-        collapse: [
-          {
-            name: "about us",
-            route: "/pages/landing-pages/about-us",
-            component: <AboutUs />,
-          },
-          {
-            name: "contact us",
-            route: "/pages/landing-pages/contact-us",
-            component: <ContactUs />,
-          },
-          {
-            name: "author",
-            route: "/pages/landing-pages/author",
-            component: <Author />,
-          },
-        ],
+        collapse: [],
       },
       {
         name: "account",
@@ -108,84 +92,82 @@ const routes = [
     ],
   },
   {
-    name: "sections",
+    name: "Research",
     icon: <Icon>view_day</Icon>,
     collapse: [
       {
-        name: "page sections",
+        name: "사람과 사물 인식",
         description: "See all sections",
         dropdown: true,
         collapse: [
           {
-            name: "page headers",
+            name: "AAM&2D-LDA를 이용한 얼굴인식",
             route: "/sections/page-sections/page-headers",
             component: <PageHeaders />,
           },
           {
-            name: "features",
+            name: "PatchMatch기반 3차원 물체인식",
+            route: "/sections/page-sections/features",
+            component: <Features />,
+          },
+          {
+            name: "PRUP-based Model",
+            route: "/sections/page-sections/features",
+            component: <Features />,
+          },
+          {
+            name: "센서정보를 활용한 스마트폰 모션 인식",
+            route: "/sections/page-sections/features",
+            component: <Features />,
+          },
+          {
+            name: "원통좌표계를 이용한 상반신 포즈 인식",
             route: "/sections/page-sections/features",
             component: <Features />,
           },
         ],
       },
       {
-        name: "navigation",
+        name: "사람과 사물 추적",
         description: "See all navigations",
         dropdown: true,
         collapse: [
           {
-            name: "navbars",
+            name: "MRF-Particle Filters",
             route: "/sections/navigation/navbars",
             component: <Navbars />,
           },
           {
-            name: "nav tabs",
+            name: "M.O.T",
             route: "/sections/navigation/nav-tabs",
             component: <NavTabs />,
           },
           {
-            name: "pagination",
+            name: "PS기반 상반신 추적 및 제스처 인식",
+            route: "/sections/navigation/pagination",
+            component: <Pagination />,
+          },
+          {
+            name: "보행자 추적기술",
             route: "/sections/navigation/pagination",
             component: <Pagination />,
           },
         ],
       },
       {
-        name: "input areas",
+        name: "실감형 콘텐츠",
         description: "See all input areas",
         dropdown: true,
         collapse: [
           {
-            name: "inputs",
+            name: "스마트폰을 이용한 증강현실 콘텐츠",
             route: "/sections/input-areas/inputs",
             component: <Inputs />,
           },
           {
-            name: "forms",
+            name: "디지펫",
             route: "/sections/input-areas/forms",
             component: <Forms />,
-          },
-        ],
-      },
-      {
-        name: "attention catchers",
-        description: "See all examples",
-        dropdown: true,
-        collapse: [
-          {
-            name: "alerts",
-            route: "/sections/attention-catchers/alerts",
-            component: <Alerts />,
-          },
-          {
-            name: "modals",
-            route: "/sections/attention-catchers/modals",
-            component: <Modals />,
-          },
-          {
-            name: "tooltips & popovers",
-            route: "/sections/attention-catchers/tooltips-popovers",
-            component: <TooltipsPopovers />,
           },
         ],
       },
@@ -239,7 +221,7 @@ const routes = [
     ],
   },
   {
-    name: "docs",
+    name: "publication",
     icon: <Icon>article</Icon>,
     collapse: [
       {
@@ -265,10 +247,238 @@ const routes = [
     ],
   },
   {
+    name: "Member",
+    icon: <Icon>article</Icon>,
+    collapse: [
+      {
+        name: "Undergraduate",
+        description: "All about overview, quick start, license and contents",
+        href: "https://www.creative-tim.com/learning-lab/react/quick-start/material-kit/",
+      },
+      {
+        name: "Master",
+        description: "See our colors, icons and typography",
+        href: "https://www.creative-tim.com/learning-lab/react/colors/material-kit/",
+      },
+      {
+        name: "Ph. D",
+        description: "Explore our collection of fully designed components",
+        href: "https://www.creative-tim.com/learning-lab/react/alerts/material-kit/",
+      },
+      {
+        name: "Graduated",
+        description: "Check how you can integrate our plugins",
+        href: "https://www.creative-tim.com/learning-lab/react/datepicker/material-kit/",
+      },
+    ],
+  },
+  {
     name: "github",
     icon: <GitHubIcon />,
     href: "https://www.github.com/creativetimofficial/material-kit-react",
   },
 ];
+// const routes = [
+//   {
+//     name: "pages",
+//     icon: <Icon>dashboard</Icon>,
+//     columns: 1,
+//     rowsPerColumn: 2,
+//     collapse: [
+//       {
+//         name: "landing pages",
+//         collapse: [
+//           {
+//             name: "about us",
+//             route: "/pages/landing-pages/about-us",
+//             component: <AboutUs />,
+//           },
+//           {
+//             name: "contact us",
+//             route: "/pages/landing-pages/contact-us",
+//             component: <ContactUs />,
+//           },
+//           {
+//             name: "author",
+//             route: "/pages/landing-pages/author",
+//             component: <Author />,
+//           },
+//         ],
+//       },
+//       {
+//         name: "account",
+//         collapse: [
+//           {
+//             name: "sign in",
+//             route: "/pages/authentication/sign-in",
+//             component: <SignIn />,
+//           },
+//         ],
+//       },
+//     ],
+//   },
+//   {
+//     name: "sections",
+//     icon: <Icon>view_day</Icon>,
+//     collapse: [
+//       {
+//         name: "page sections",
+//         description: "See all sections",
+//         dropdown: true,
+//         collapse: [
+//           {
+//             name: "page headers",
+//             route: "/sections/page-sections/page-headers",
+//             component: <PageHeaders />,
+//           },
+//           {
+//             name: "features",
+//             route: "/sections/page-sections/features",
+//             component: <Features />,
+//           },
+//         ],
+//       },
+//       {
+//         name: "navigation",
+//         description: "See all navigations",
+//         dropdown: true,
+//         collapse: [
+//           {
+//             name: "navbars",
+//             route: "/sections/navigation/navbars",
+//             component: <Navbars />,
+//           },
+//           {
+//             name: "nav tabs",
+//             route: "/sections/navigation/nav-tabs",
+//             component: <NavTabs />,
+//           },
+//           {
+//             name: "pagination",
+//             route: "/sections/navigation/pagination",
+//             component: <Pagination />,
+//           },
+//         ],
+//       },
+//       {
+//         name: "input areas",
+//         description: "See all input areas",
+//         dropdown: true,
+//         collapse: [
+//           {
+//             name: "inputs",
+//             route: "/sections/input-areas/inputs",
+//             component: <Inputs />,
+//           },
+//           {
+//             name: "forms",
+//             route: "/sections/input-areas/forms",
+//             component: <Forms />,
+//           },
+//         ],
+//       },
+//       {
+//         name: "attention catchers",
+//         description: "See all examples",
+//         dropdown: true,
+//         collapse: [
+//           {
+//             name: "alerts",
+//             route: "/sections/attention-catchers/alerts",
+//             component: <Alerts />,
+//           },
+//           {
+//             name: "modals",
+//             route: "/sections/attention-catchers/modals",
+//             component: <Modals />,
+//           },
+//           {
+//             name: "tooltips & popovers",
+//             route: "/sections/attention-catchers/tooltips-popovers",
+//             component: <TooltipsPopovers />,
+//           },
+//         ],
+//       },
+//       {
+//         name: "elements",
+//         description: "See all 32 examples",
+//         dropdown: true,
+//         collapse: [
+//           {
+//             name: "avatars",
+//             route: "/sections/elements/avatars",
+//             component: <Avatars />,
+//           },
+//           {
+//             name: "badges",
+//             route: "/sections/elements/badges",
+//             component: <Badges />,
+//           },
+//           {
+//             name: "breadcrumbs",
+//             route: "/sections/elements/breadcrumbs",
+//             component: <BreadcrumbsEl />,
+//           },
+//           {
+//             name: "buttons",
+//             route: "/sections/elements/buttons",
+//             component: <Buttons />,
+//           },
+//           {
+//             name: "dropdowns",
+//             route: "/sections/elements/dropdowns",
+//             component: <Dropdowns />,
+//           },
+//           {
+//             name: "progress bars",
+//             route: "/sections/elements/progress-bars",
+//             component: <ProgressBars />,
+//           },
+//           {
+//             name: "toggles",
+//             route: "/sections/elements/toggles",
+//             component: <Toggles />,
+//           },
+//           {
+//             name: "typography",
+//             route: "/sections/elements/typography",
+//             component: <Typography />,
+//           },
+//         ],
+//       },
+//     ],
+//   },
+//   {
+//     name: "docs",
+//     icon: <Icon>article</Icon>,
+//     collapse: [
+//       {
+//         name: "getting started",
+//         description: "All about overview, quick start, license and contents",
+//         href: "https://www.creative-tim.com/learning-lab/react/quick-start/material-kit/",
+//       },
+//       {
+//         name: "foundation",
+//         description: "See our colors, icons and typography",
+//         href: "https://www.creative-tim.com/learning-lab/react/colors/material-kit/",
+//       },
+//       {
+//         name: "components",
+//         description: "Explore our collection of fully designed components",
+//         href: "https://www.creative-tim.com/learning-lab/react/alerts/material-kit/",
+//       },
+//       {
+//         name: "plugins",
+//         description: "Check how you can integrate our plugins",
+//         href: "https://www.creative-tim.com/learning-lab/react/datepicker/material-kit/",
+//       },
+//     ],
+//   },
+//   {
+//     name: "github",
+//     icon: <GitHubIcon />,
+//     href: "https://www.github.com/creativetimofficial/material-kit-react",
+//   },
+// ];
 
 export default routes;
