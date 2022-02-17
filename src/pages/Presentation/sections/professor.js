c7e78742d729f85846d04c4487169c7f1ac1af96
나광일 <nagwangil@MacBookAir.local>
2022-02-17 17:16:07 +0900

    update professor.js

--- src/pages/Presentation/sections/professor.js
+++ src/pages/Presentation/sections/professor.js
@@ -2,7 +2,7 @@
 
 import Container from "@mui/material/Container";
 import Grid from "@mui/material/Grid";
-import Icon from "@mui/material/Icon";
+// import Icon from "@mui/material/Icon";
 import Stack from "@mui/material/Stack";
 
 // Material Kit 2 React components
@@ -28,7 +28,7 @@ function Professor() {
             <MKTypography variant="h3" my={1}>
               Prof. Chill Woo LEE
             </MKTypography>
-            <MKTypography variant="body2" color="text" mb={2}>
+            <MKTypography color="text" mb={2} pr={5}>
               Pain is what we go through as we become older. We get insulted by others, lose trust
               for those others. We get back stabbed by friends. It becomes harder for us to give
               others a hand.
@@ -37,71 +37,104 @@ function Professor() {
           <Grid item xs={12} lg={6} sx={{ ml: { xs: -2, lg: "auto" }, mt: { xs: 6, lg: 0 } }}>
             <Stack justifyContent="center">
               <MKBox justifyContent="center">
-                <MKTypography variant="h2" textTransform="capitalize">
-                  Educations
+                <MKTypography variant="h2" textTransform="capitalize" mb={2}>
+                  educations
                 </MKTypography>
               </MKBox>
-              <MKBox display="flex" alignItems="center" p={2}>
+
+              <MKBox display="flex" alignItems="center" p={1}>
                 <MKBox
-                  width="3rem"
-                  height="3rem"
+                  width="1rem"
+                  height="1rem"
                   variant="gradient"
-                  bgColor="info"
-                  color="white"
+                  color="black"
                   coloredShadow="info"
+                  bgColor="dark"
                   display="flex"
                   alignItems="center"
                   justifyContent="center"
                   borderRadius="xl"
-                >
-                  <Icon fontSize="small">mediation</Icon>
-                </MKBox>
-                <MKTypography variant="body2" color="text" pl={2}>
-                  It becomes harder for us to give others a hand.
-                  <br />
-                  We get our heart broken by people we love.
+                />
+                <MKTypography color="text" pl={1}>
+                  B.D. in electric engineering chung-ang university, Seoul, Korea
+                  <br /> - Feb. 1986
                 </MKTypography>
               </MKBox>
-              <MKBox display="flex" alignItems="center" p={2}>
+              <MKBox display="flex" alignItems="center" p={1}>
                 <MKBox
-                  width="3rem"
-                  height="3rem"
+                  width="1rem"
+                  height="1rem"
                   variant="gradient"
-                  bgColor="info"
-                  color="white"
+                  color="black"
                   coloredShadow="info"
+                  bgColor="dark"
                   display="flex"
                   alignItems="center"
                   justifyContent="center"
                   borderRadius="xl"
-                >
-                  <Icon fontSize="small">settings_overscan</Icon>
-                </MKBox>
-                <MKTypography variant="body2" color="text" pl={2}>
-                  As we live, our hearts turn colder.
-                  <br />
-                  Cause pain is what we go through as we become older.
+                />
+                <MKTypography color="text" pl={1}>
+                  Ph.D in electric engineering Tokyo University, Tokyo, Japan <br />- Mar. 1992
                 </MKTypography>
               </MKBox>
-              <MKBox display="flex" alignItems="center" p={2}>
+
+              <MKBox justifyContent="center">
+                <MKTypography variant="h2" textTransform="capitalize" mb={2} mt={4}>
+                  experience
+                </MKTypography>
+              </MKBox>
+              <MKBox display="flex" alignItems="center" p={1}>
                 <MKBox
-                  width="3rem"
-                  height="3rem"
+                  width="1rem"
+                  height="1rem"
                   variant="gradient"
-                  bgColor="info"
-                  color="white"
+                  color="black"
                   coloredShadow="info"
+                  bgColor="dark"
                   display="flex"
                   alignItems="center"
                   justifyContent="center"
                   borderRadius="xl"
-                >
-                  <Icon fontSize="small">token</Icon>
-                </MKBox>
-                <MKTypography variant="body2" color="text" pl={2}>
-                  When we lose family over time.
-                  <br />
-                  What else could rust the heart more over time? Blackgold.
+                />
+                <MKTypography color="text" pl={1}>
+                  Head of Information and Communication Research Institute <br /> Apr. 2006 ~ Mar.
+                  2008
+                </MKTypography>
+              </MKBox>
+              <MKBox display="flex" alignItems="center" p={1}>
+                <MKBox
+                  min-width="1rem"
+                  min-height="1rem"
+                  variant="gradient"
+                  color="black"
+                  coloredShadow="info"
+                  bgColor="dark"
+                  display="flex"
+                  alignItems="center"
+                  justifyContent="center"
+                  borderRadius="xl"
+                />
+                <MKTypography color="text" pl={1}>
+                  Head of Culture Contents Technology Research Institute at Chonnam National
+                  University <br /> Mar. 2006 ~
+                </MKTypography>
+              </MKBox>
+              <MKBox display="flex" alignItems="center" p={1}>
+                <MKBox
+                  width="1rem"
+                  height="1rem"
+                  variant="gradient"
+                  color="black"
+                  coloredShadow="info"
+                  bgColor="dark"
+                  display="flex"
+                  alignItems="center"
+                  justifyContent="center"
+                  borderRadius="xl"
+                />
+                <MKTypography color="text" pl={1}>
+                  Professor of Electronic Computer Engineering at Chonnam National University.
+                  <br /> Jan. 1996 ~
                 </MKTypography>
               </MKBox>
             </Stack>
